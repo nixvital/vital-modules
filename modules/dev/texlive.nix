2c8cd8e65d72c0aee81c62cc318b1572cd426a67
Break Yang <breakds@gmail.com>
2022-07-05 09:43:10 -0700

    Install full texlive, because I can

--- modules/dev/texlive.nix
+++ modules/dev/texlive.nix
@@ -2,19 +2,12 @@
 
 let cfg = config.vital.programs.texlive;
 
-    customized-texlive = pkgs.texlive.combine {
-      inherit (pkgs.texlive) collection-basic collection-latex collection-latexextra
-        collection-latexrecommended collection-fontsrecommended collection-langchinese
-        collection-langcjk collection-metapost collection-bibtexextra
-        newlfm dvipng;
-    };
-
 in {
   options.vital.programs.texlive = with lib; {
     enable = mkEnableOption "Enable texlive suite for TeX development";
   };
   
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ customized-texlive ];
+    environment.systemPackages = [ pkgs.texlive.combined.scheme-full ];
   };
 }
