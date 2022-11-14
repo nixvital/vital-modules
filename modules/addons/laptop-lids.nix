2db3ee55bd347103a19fa040a59970f02bf30d36
Break Yang <breakds@gmail.com>
2022-11-14 10:45:03 -0800

    Update power saving mode for laptop suspend

--- modules/addons/laptop-lids.nix
+++ modules/addons/laptop-lids.nix
@@ -10,7 +10,7 @@
     #   1. If the external power is on, do nothing
     #   2. If the laptop is docked (external dock or monitor or hub), do nothing
     #   3. Otherwise, it should go to suspend and then hibernate. However this action
-    #      will be held off for 20 seconds to wait for the users to dock or plug
+    #      will be held off for 60 seconds to wait for the users to dock or plug
     #      external power.
     lidSwitch = "suspend-then-hibernate";
     lidSwitchDocked = "ignore";
@@ -19,4 +19,10 @@
         HoldoffTimeoutSec=60
       '';
   };
+
+  # Suspend-to-RAM. This state, if supported, offers significant power savings
+  # as everything in the system is put into a low-power state, except for
+  # memory, which should be placed into the self-refresh mode to retain its
+  # contents.
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
 }
