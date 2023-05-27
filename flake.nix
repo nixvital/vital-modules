c128061a322129218d89678c63a40fdde99467ac
Break Yang <breakds@gmail.com>
2023-05-27 14:45:50 -0700

    Remove old dependencies

--- flake.nix
+++ flake.nix
@@ -2,27 +2,14 @@
   description = "A collection of NixOS modules serving as building blocks to construct NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs?rev=f924460e91cba6473c5dc4b8ccb1a1cfc05bc2d7";
-    
     # Use vitalpkgs, with the same nixpkgs
     vitalpkgs.url = "github:nixvital/vitalpkgs";
     vitalpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, vitalpkgs, ... }: let
-    withVitalpkgs = module : {config, lib, pkgs, ...} : {
-      imports = [ module ];
-      nixpkgs.overlays = [
-        vitalpkgs.overlay
-        (final: prev: {
-          chia = nixpkgs-unstable.legacyPackages."${prev.system}".chia;
-        })
-      ];
-    };
-
-  in {
+  outputs = { self, nixpkgs, vitalpkgs, ... }: {
     # This is to enable building the livecd iso with `nix build .#livecd`
     packages."x86_64-linux" = {
       livecd = self.nixosConfigurations.livecd.config.system.build.isoImage;
@@ -31,8 +18,8 @@
     nixosModules = {
       # Import this so that you have a backbone that you can build
       # your machine up on.
-      foundation = withVitalpkgs (import ./foundations);
-      container-foundation = withVitalpkgs (import ./foundations/container.nix);
+      foundation = import ./foundations;
+      container-foundation = import ./foundations/container.nix;
       
       # Individual Modules
       graphical = import ./modules/graphical;
@@ -64,9 +51,9 @@
       # to test the build
       test-vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = let
-          test-vm = withVitalpkgs (import ./machines/test-vm.nix);
-        in [ test-vm ];
+        modules = [
+          ./machines/test-vm.nix
+        ];
       };
 
       test-container = nixpkgs.lib.nixosSystem {
