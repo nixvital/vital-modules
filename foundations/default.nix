f1ab61f37ee6f7caf5ec9445c58e10f99003e662
Break Yang <breakds@gmail.com>
2023-05-27 15:49:06 -0700

    Fix X11 forwarding

--- foundations/default.nix
+++ foundations/default.nix
@@ -59,7 +59,7 @@
   services.openssh = {
     enable = true;
     # Enable X11 Fowarding, can be connected with ssh -Y.
-    settings.forwardX11 = true;
+    settings.X11Forwarding = true;
     # TODO(breakds): Enable this for servers
     # allowSFTP = config.vital.machineType == "server";
   };
