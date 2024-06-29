001323d258debc9a1002062ad6bb28aa9b8ab194
Break Yang <breakds@gmail.com>
2024-06-29 12:27:54 -0700

    USe 7.4 instead of 8.1

--- modules/services/filerun.nix
+++ modules/services/filerun.nix
@@ -86,7 +86,7 @@ in {
 
       # The backend and web app (Filerun)
       virtualisation.oci-containers.containers."filerun" = {
-        image = "filerun/filerun:8.1";
+        image = "filerun/filerun:7.4";
         environment = {
           "FR_DB_HOST" = "${dbContainerHost}";
           # This is the default port that mariadb runs at.
